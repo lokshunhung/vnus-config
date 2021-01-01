051ced2a53b55008cc4c1fbb9a292a2ab75128c8
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-01-01 21:48:52 +0800

    refactor: remove babel

--- tools/eslint-config-vnus/rollup.config.js
+++ tools/eslint-config-vnus/rollup.config.js
@@ -1,11 +1,25 @@
-import pluginBabel from '@rollup/plugin-babel';
 import pluginCommonjs from '@rollup/plugin-commonjs';
 import pluginNodeResolve from '@rollup/plugin-node-resolve';
 import pluginTypescript from '@rollup/plugin-typescript';
 import * as path from 'path';
+import * as prettier from 'prettier';
+import { terser as pluginTerser } from 'rollup-plugin-terser';
 
 /** @param {string} p */
 const joinRoot = (p) => path.join(__dirname, '../../', p);
+
+/** @returns {import("rollup").Plugin} */
+const pluginPrettierFormatOutput = () => {
+    /** @type {prettier.Options | null} */
+    let prettierOptions;
+    return {
+        name: 'prettier-format-output',
+        async renderChunk(code, chunk, options) {
+            if (!prettierOptions) prettierOptions = await prettier.resolveConfig(process.cwd());
+            return { code: prettier.format(code, { ...prettierOptions, filepath: chunk.fileName }) };
+        },
+    };
+};
 
 /** @type {import('rollup').RollupOptions} */
 const config = {
@@ -21,17 +35,16 @@ const config = {
     plugins: [
         pluginNodeResolve(),
         pluginCommonjs(),
-        pluginBabel({
-            include: ['**/node_modules/confusing-browser-globals/**'],
-            babelrc: false,
-            babelHelpers: 'bundled',
-            comments: false,
-        }),
         pluginTypescript({
             tsconfig: joinRoot('tsconfig.json'),
             module: 'ESNext',
             removeComments: true,
         }),
+        pluginTerser({
+            mangle: false,
+            compress: false,
+        }),
+        pluginPrettierFormatOutput(),
     ],
 };
 
