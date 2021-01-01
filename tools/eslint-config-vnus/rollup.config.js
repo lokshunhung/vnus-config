6668a42c2b1d23b0716cf4dbfa714411e52eab05
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-01-02 00:13:51 +0800

    refactor: refactor rollup inputs

--- tools/eslint-config-vnus/rollup.config.js
+++ tools/eslint-config-vnus/rollup.config.js
@@ -2,6 +2,7 @@ import pluginCommonjs from '@rollup/plugin-commonjs';
 import pluginNodeResolve from '@rollup/plugin-node-resolve';
 import pluginReplace from '@rollup/plugin-replace';
 import pluginTypescript from '@rollup/plugin-typescript';
+import * as fs from 'fs';
 import * as path from 'path';
 import { terser as pluginTerser } from 'rollup-plugin-terser';
 import { pluginAddMITLicenseFile, pluginGeneratePkgJsonWithDependencies, pluginPrettierFormatOutput } from '../utils';
@@ -12,15 +13,19 @@ const joinRoot = (...p) => path.join(__dirname, '../../', ...p);
 /** @param {string[]} p */
 const joinPkg = (...p) => path.relative(process.cwd(), joinRoot(`packages/eslint-config-vnus/`, ...p));
 
+/** @type {Record<string, string>} */
+const inputTSFiles = {};
+fs.readdirSync(joinPkg(''), { encoding: 'utf8', withFileTypes: true })
+    .filter((e) => e.isFile())
+    .filter((e) => e.name.endsWith('.ts'))
+    .forEach((e) => {
+        inputTSFiles[e.name.replace(/\.ts$/, '')] = joinPkg(e.name);
+    });
+
 /** @type {import('rollup').RollupOptions} */
 const config = {
     input: {
-        'eslint-recommended': joinPkg('eslint-recommended.ts'),
-        jest: joinPkg('jest.ts'),
-        'prettier-eslint': joinPkg('prettier-eslint.ts'),
-        'prettier-typescript-eslint': joinPkg('prettier-typescript-eslint.ts'),
-        'typescript-eslint-recommended': joinPkg('typescript-eslint-recommended.ts'),
-        'typescript-eslint-with-requiring-type-checking': joinPkg('typescript-eslint-with-requiring-type-checking.ts'),
+        ...inputTSFiles,
     },
     output: {
         dir: joinRoot('dist/eslint-config-vnus'),
