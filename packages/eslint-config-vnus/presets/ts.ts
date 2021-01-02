ad2bbe0142d9dc01176053d55323c38f66b9bb53
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-01-02 22:21:27 +0800

    feat: add ts preset

---
+++ packages/eslint-config-vnus/presets/ts.ts
@@ -0,0 +1,45 @@
+import type { Linter } from 'eslint';
+
+declare module 'eslint' {
+    namespace Linter {
+        interface ParserOptions {
+            jsxPragma?: string;
+            jsxFragmentName?: string | null;
+            lib?: string[];
+
+            project?: string | string[];
+            projectFolderIgnoreList?: string[];
+            tsconfigRootDir?: string;
+            extraFileExtensions?: string[];
+            warnOnUnsupportedTypeScriptVersion?: boolean;
+        }
+    }
+}
+
+const _require = require;
+
+const config: Linter.Config = {
+    plugins: ['@typescript-eslint', 'eslint-comments'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2018,
+        ecmaFeatures: { jsx: true },
+        project: './tsconfig.*?.json',
+    },
+    settings: {
+        react: { version: 'detect' },
+    },
+    env: {
+        node: true,
+    },
+    rules: {
+        ..._require('../rules/eslint-recommended').rules,
+        ..._require('../rules/typescript-eslint-recommended').rules,
+        ..._require('../rules/eslint-comments-recommended').rules,
+        ..._require('../rules/prettier-eslint').rules,
+        ..._require('../rules/prettier-typescript-eslint').rules,
+    },
+};
+
+export default config;
