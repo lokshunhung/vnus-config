2a667091be6639759993df89760cc7889845c86c
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-01-04 18:45:24 +0800

    feat: add library preset

--- packages/eslint-config-vnus/presets/library.ts
+++ packages/eslint-config-vnus/presets/library.ts
@@ -1,5 +1,21 @@
 import type { ESLintConfig } from '../../types';
 
-const config: ESLintConfig = {};
+const config: ESLintConfig = {
+    extends: [require.resolve('../rules/eslint-recommended'), require.resolve('../rules/prettier-eslint')],
+    overrides: [
+        {
+            files: ['*.ts'],
+            extends: [
+                require.resolve('../rules/typescript-eslint-recommended'),
+                require.resolve('../rules/typescript-eslint-requiring-type-checking'),
+                require.resolve('../rules/prettier-typescript-eslint'),
+            ],
+        },
+        {
+            files: ['*.spec.js', '*.spec.ts'],
+            extends: [require.resolve('../rules/jest-recommended')],
+        },
+    ],
+};
 
 export default config;
