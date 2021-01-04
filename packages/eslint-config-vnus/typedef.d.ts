76e0f0754102e839fcecff9f28915da77ceda8b0
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-01-04 17:42:35 +0800

    fix: correct eslint typedef

--- packages/eslint-config-vnus/typedef.d.ts
+++ packages/eslint-config-vnus/typedef.d.ts
@@ -1,23 +1,23 @@
 declare module '@typescript-eslint/eslint-plugin/dist/configs/eslint-recommended' {
     import type { Linter } from 'eslint';
     declare const _default: Linter.Config;
-    export default _default;
-}
-
-declare module 'eslint-config-prettier/react' {
-    import type { Linter } from 'eslint';
-    declare const _default: Linter.Config;
-    export default _default;
-}
-
-declare module 'eslint-config-prettier/index' {
-    import type { Linter } from 'eslint';
-    declare const _default: Linter.Config;
-    export default _default;
+    export = _default;
 }
 
 declare module 'eslint-config-prettier/@typescript-eslint' {
     import type { Linter } from 'eslint';
     declare const _default: Linter.Config;
-    export default _default;
+    export = _default;
+}
+
+declare module 'eslint-config-prettier/index' {
+    import type { Linter } from 'eslint';
+    declare const _default: Linter.Config;
+    export = _default;
+}
+
+declare module 'eslint-config-prettier/react' {
+    import type { Linter } from 'eslint';
+    declare const _default: Linter.Config;
+    export = _default;
 }
