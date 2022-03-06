9ffdb4e88f29bd3767c0bedc57e05be91d54093e
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2022-03-06 21:54:53 +0800

    refactor: wip eslint possible problems

--- packages/eslint-config-vnus/rules/eslint/possible-errors.ts
+++ packages/eslint-config-vnus/rules/eslint/possible-errors.ts
@@ -1,4 +1,4 @@
-import type { Linter } from "eslint";
+// @ts-check
 
 // ✅ Recommended
 // 🔧 Fixable
@@ -6,12 +6,16 @@ import type { Linter } from "eslint";
 // ✨ Customized
 // 🛑 Disabled
 
-const rulesPossibleErrors: Linter.Config = {
+/** @type {import("eslint").Linter.Config} */
+const rulesPossibleProblems = {
     rules: {
+        // ✅ require `super()` calls in constructors
+        "constructor-super": ["error"],
+
         // ✅ enforce "for" loop update clause moving the counter in the right direction.
         "for-direction": ["error"],
 
-        // ✅ enforce `return` statements in getters
+        // ✨ ✅ enforce `return` statements in getters
         "getter-return": ["error", { allowImplicit: true }],
 
         // ✅ disallow using an async function as a Promise executor
@@ -20,14 +24,17 @@ const rulesPossibleErrors: Linter.Config = {
         // ✨ disallow `await` inside of loops
         "no-await-in-loop": ["error"],
 
+        // ✅ disallow reassigning class members
+        "no-class-assign": ["error"],
+
         // ✅ disallow comparing against -0
         "no-compare-neg-zero": ["error"],
 
         // ✨ ✅ disallow assignment operators in conditional expressions
         "no-cond-assign": ["error", "always"],
 
-        // ✨ warns about the use of `console`; allow `console.info`, `console.warn`, `console.error`, `console.time`, `console.timeLog`, `console.timeEnd`
-        "no-console": ["warn", { allow: ["info", "warn", "error", "time", "timeLog", "timeEnd"] }],
+        // ✅ disallow reassigning `const` variables
+        "no-const-assign": ["error"],
 
         // ✅ disallow constant expressions in conditions
         "no-constant-condition": ["error"],
@@ -41,6 +48,9 @@ const rulesPossibleErrors: Linter.Config = {
         // ✅ disallow duplicate arguments in `function` definitions
         "no-dupe-args": ["error"],
 
+        // ✅ disallow duplicate class members
+        "no-dupe-class-members": ["error"],
+
         // ✅ disallow duplicate conditions in if-else-if chains
         "no-dupe-else-if": ["error"],
 
@@ -50,20 +60,20 @@ const rulesPossibleErrors: Linter.Config = {
         // ✅ disallow duplicate case labels
         "no-duplicate-case": ["error"],
 
-        // ✅ disallow empty block statements
-        "no-empty": ["error"],
+        // 🛑 use "import/no-duplicates"
+        "no-duplicate-imports": "off",
 
         // ✅ disallow empty character classes in regular expressions
         "no-empty-character-class": ["error"],
 
+        // ✅ disallow empty destructuring patterns
+        "no-empty-pattern": ["error"],
+
         // ✅ disallow reassigning exceptions in `catch` clauses
         "no-ex-assign": ["error"],
 
-        // ✅ 🔧 disallow unnecessary boolean casts
-        "no-extra-boolean-cast": ["error"],
-
-        // ✅ 🔧 disallow unnecessary semicolons
-        "no-extra-semi": ["error"],
+        // ✅ disallow fallthrough of `case` statements
+        "no-fallthrough": ["error"],
 
         // ✅ disallow reassigning `function` declarations
         "no-func-assign": ["error"],
@@ -80,11 +90,14 @@ const rulesPossibleErrors: Linter.Config = {
         // ✅ disallow irregular whitespace
         "no-irregular-whitespace": ["error"],
 
-        // ✨ disallow literal numbers that lose precision
+        // ✅ disallow literal numbers that lose precision
         "no-loss-of-precision": ["error"],
 
         // ✅ disallow characters which are made with multiple code points in character class syntax
         "no-misleading-character-class": ["error"],
+
+        // ✅ disallow `new` operators with the `Symbol` object
+        "no-new-symbol": ["error"],
 
         // ✅ disallow calling global object properties as functions
         "no-obj-calls": ["error"],
@@ -92,14 +105,20 @@ const rulesPossibleErrors: Linter.Config = {
         // ✅ disallow calling some `Object.prototype` methods directly on objects
         "no-prototype-builtins": ["error"],
 
-        // ✅ 🔧 disallow multiple spaces in regular expressions
-        "no-regex-spaces": ["error"],
+        // ✅ disallow assignments where both sides are exactly the same
+        "no-self-assign": ["error"],
 
         // ✅ disallow returning values from setters
         "no-setter-return": ["error"],
 
         // ✅ disallow sparse arrays
         "no-sparse-arrays": ["error"],
+
+        // ✅ disallow `this`/`super` before calling `super()` in constructors
+        "no-this-before-super": ["error"],
+
+        // ✅ disallow the use of undeclared variables unless mentioned in `/*global */` comments
+        "no-undef": ["error"],
 
         // ✅ disallow confusing multiline expressions
         "no-unexpected-multiline": ["error"],
@@ -113,8 +132,29 @@ const rulesPossibleErrors: Linter.Config = {
         // ✅ disallow negating the left operand of relational operators
         "no-unsafe-negation": ["error"],
 
-        // ✨ disallow use of optional chaining in contexts where the `undefined` value is not allowed
+        // ✅ disallow use of optional chaining in contexts where the `undefined` value is not allowed
         "no-unsafe-optional-chaining": ["error", { disallowArithmeticOperators: true }],
+
+        // ✨ ✅ disallow unused variables; re-disabled by "@typescript-eslint/no-unused-vars"
+        "no-unused-vars": [
+            "error",
+            {
+                args: "after-used",
+                argsIgnorePattern: "^_",
+                ignoreRestSiblings: true,
+                varsIgnorePattern: "^_",
+            },
+        ],
+
+        // ✨ ✅ disallow the use of variables before they are defined; re-disabled by "@typescript-eslint/no-use-before-define"
+        "no-use-before-define": [
+            "error",
+            {
+                functions: false,
+                classes: true,
+                variables: true,
+            },
+        ],
 
         // ✅ require calls to `isNaN()` when checking for `NaN`
         "use-isnan": ["error"],
@@ -124,4 +164,4 @@ const rulesPossibleErrors: Linter.Config = {
     },
 };
 
-export default rulesPossibleErrors;
+module.exports = rulesPossibleProblems;
